0910b2e55e4d57cef89e622b9516b807aa050c3e
Maria Groves <mariagrovesh@gmail.com>
2020-05-27 17:25:54 +0200

    made the about page and footer

--- app/javascript/components/scroll.js
+++ app/javascript/components/scroll.js
@@ -1,10 +1,15 @@
-const button = document.querySelector('.fa-chevron-down');
-button.addEventListener('click', (event) => {
+const buttonHero = document.querySelector('.scroll-hero');
+const buttonFeatures = document.querySelector('.scroll-features');
+buttonHero.addEventListener('click', (event) => {
   document.querySelector('.features').scrollIntoView({ 
     behavior: 'smooth' 
   });
 });
-
+buttonFeatures.addEventListener('click', (event) => {
+  document.querySelector('.about').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
+});
 
 
 // this function runs when the DOM is ready, i.e. when the document has been parsed
