3bb2cd1a565ea756fa177f61597a1be0f1b48a45
Maria Groves <mariagrovesh@gmail.com>
2020-08-04 11:45:28 +0200

    added dep destroy and potential code for destroying orphaned cart

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,11 +1,20 @@
 class OrderItem < ApplicationRecord
   belongs_to :menu_item
-  has_many :order_item_options
+  has_many :order_item_options, dependent: :destroy
   has_many :menu_options, through: :order_item_options
   belongs_to :cart
   accepts_nested_attributes_for :order_item_options
   before_save :set_unit_price
   before_save :set_total
+  # around_destroy :destroy_orphaned_cart
+
+  # def destroy_orphaned_cart
+  #   cart = self.cart
+  #   yield # executes a DELETE database statement
+  #   if cart.order_items.length == 0
+  #     cart.destroy
+  #   end  
+  # end
 
   def unit_price
     self.menu_item.price
