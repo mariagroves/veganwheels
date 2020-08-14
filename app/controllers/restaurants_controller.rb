9525d46060933ec5ba995a7e15c504c9fb8a3d77
Maria Groves <mariagrovesh@gmail.com>
2020-08-14 12:52:19 +0200

    fixed restaurant search bug with postcodes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,10 +5,20 @@ class RestaurantsController < ApplicationController
         @restaurants = Restaurant.all
         @search = params["search"]
         if @search.present?
-            @postcode = @search["postcode"] # doesnt work if no space in PC
-            @results = Geocoder.search(@postcode)
-            @coordinates = @results.first.coordinates
-            @restaurants = Restaurant.near(@coordinates, 5, units: :km)
+            @postcode = @search["postcode"] # "G327QR"
+            @results_array = []
+
+            @results_array << @results = Geocoder.search(@postcode)
+            @results_array << @results2 = Geocoder.search(@postcode.split(//).insert(2, ' ').join)
+            @results_array << @results3 = Geocoder.search(@postcode.split(//).insert(3, ' ').join)
+        
+            @results_array.each do |result|
+                if !result.empty? && result.first.country == "United Kingdom"
+                    @coordinates = result.first.coordinates
+                end
+            end
+
+            @restaurants = Restaurant.near(@coordinates, 5, units: :km).sort_by(&:id)
         end
     end
 
