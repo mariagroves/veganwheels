b3d53010df3eec4b55f34a9ce3d1675f71686142
Maria Groves <mariagrovesh@gmail.com>
2020-09-09 13:10:21 +0200

    fix for seed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,7 +6,7 @@ class Restaurant < ApplicationRecord
     validates :name, presence: true
     validates :address, presence: true
     validates :about, presence: true
-    validates :photo, presence: true
+    # validates :photo, presence: true
     geocoded_by :address
     after_validation :geocode, if: :will_save_change_to_address?
 end
