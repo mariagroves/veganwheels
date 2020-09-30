8c290da5dfbc47b6c1f9109feec388fb43063372
Maria Groves <mariagrovesh@gmail.com>
2020-09-30 13:27:47 +0200

    fixed seed

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ module Veganwheels
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.active_record.time_zone_aware_types = [:datetime]
     config.time_zone = "London"
+    config.active_record.time_zone_aware_types = [:datetime]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
