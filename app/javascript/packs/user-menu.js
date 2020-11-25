12ab5108596ee4d8f1b1aefeddcff5f7cd8591dd
Maria Groves <mariagrovesh@gmail.com>
2020-11-25 13:19:09 +0100

    responsive user icon in nav

--- app/javascript/packs/user-menu.js
+++ app/javascript/packs/user-menu.js
@@ -1,13 +1,32 @@
-const navSection = document.getElementById('user_icon');
+const navSection1 = document.getElementById('user_icon_1');
+const navSection2 = document.getElementById('user_icon_2');
 
-navSection.addEventListener("click", () => {
-    let userMenu = document.querySelector('#user_icon #user_menu');
+if (window.innerWidth < 1199) {
     
-    if (document.querySelectorAll('#user_icon #user_menu').length != 0) {
-        if (userMenu.style.display === "none") {
-            userMenu.style.display = "block";
-        } else {
-            userMenu.style.display = "none";
-        }
-    };
-})
+    navSection1.addEventListener("click", () => {
+        let userMenu = document.querySelector('#user_icon_1 #user_menu');
+        
+        if (document.querySelectorAll('#user_icon_1 #user_menu').length != 0) {
+            if (userMenu.style.display === "none") {
+                userMenu.style.display = "block";
+            } else {
+                userMenu.style.display = "none";
+            }
+        };
+    })
+
+} else {
+    console.log("else")
+    navSection2.addEventListener("click", () => {
+        let userMenu = document.querySelector('#user_icon_2 #user_menu');
+        
+        if (document.querySelectorAll('#user_icon_2 #user_menu').length != 0) {
+            if (userMenu.style.display === "none") {
+                userMenu.style.display = "block";
+            } else {
+                userMenu.style.display = "none";
+            }
+        };
+    })
+}
+
