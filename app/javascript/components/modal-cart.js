31fffd335e4364009e819e8403389539bc9c765b
Maria Groves <mariagrovesh@gmail.com>
2020-11-26 15:39:39 +0100

    responsive nav

--- app/javascript/components/modal-cart.js
+++ app/javascript/components/modal-cart.js
@@ -1,11 +1,42 @@
-const navSection = document.getElementById('nav_section');
+const navSection1 = document.getElementById('nav_section_1');
+const navSection2 = document.getElementById('nav_section_2');
+const navSection3 = document.getElementById('nav_section_3');
+
 const modalBg = document.getElementsByClassName('modal-cart')[0];
-navSection.addEventListener("click", () => {
-    if (document.querySelectorAll('#nav_section .nav_cart').length != 0) {
-        modalBg.classList.add('bckg-active');
-        const modalCancelButton = document.getElementById('cart-modal-close');
-        modalCancelButton.addEventListener('click', () => {
-            modalBg.classList.remove('bckg-active');
-        })
-    };
-})
+
+if (window.innerWidth > 575 && window.innerWidth <= 1199) {
+    
+    navSection1.addEventListener("click", () => {
+        if (document.querySelectorAll('#nav_section_1 .nav_cart').length != 0) {
+            modalBg.classList.add('bckg-active');
+            const modalCancelButton = document.getElementById('cart-modal-close');
+            modalCancelButton.addEventListener('click', () => {
+                modalBg.classList.remove('bckg-active');
+            })
+        };
+    })
+
+} else if (window.innerWidth <= 575) {
+
+    navSection3.addEventListener("click", () => {
+        if (document.querySelectorAll('#nav_section_3 .nav_cart').length != 0) {
+            modalBg.classList.add('bckg-active');
+            const modalCancelButton = document.getElementById('cart-modal-close');
+            modalCancelButton.addEventListener('click', () => {
+                modalBg.classList.remove('bckg-active');
+            })
+        };
+    })
+
+} else {
+
+    navSection2.addEventListener("click", () => {
+        if (document.querySelectorAll('#nav_section_2 .nav_cart').length != 0) {
+            modalBg.classList.add('bckg-active');
+            const modalCancelButton = document.getElementById('cart-modal-close');
+            modalCancelButton.addEventListener('click', () => {
+                modalBg.classList.remove('bckg-active');
+            })
+        };
+    })
+}
--- app/javascript/packs/user-menu.js
+++ app/javascript/packs/user-menu.js
@@ -1,7 +1,7 @@
 const navSection1 = document.getElementById('user_icon_1');
 const navSection2 = document.getElementById('user_icon_2');
 
-if (window.innerWidth < 1199) {
+if (window.innerWidth <= 1199) {
     
     navSection1.addEventListener("click", () => {
         let userMenu = document.querySelector('#user_icon_1 #user_menu');
@@ -16,7 +16,6 @@ if (window.innerWidth < 1199) {
     })
 
 } else {
-    console.log("else")
     navSection2.addEventListener("click", () => {
         let userMenu = document.querySelector('#user_icon_2 #user_menu');
         
