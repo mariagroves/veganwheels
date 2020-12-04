50467e0c49b0b704089171883f8d82ab7a4efa27
Maria Groves <mariagrovesh@gmail.com>
2020-12-04 16:19:01 +0100

    added cart tests

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "test that the cart subtotal is returned correctly" do
+    assert_equal 3700, carts(:one).get_subtotal
+  end
+
+  test "test that the cart total is returned correctly" do
+    assert_equal 3800, carts(:one).get_total
+  end
+
+  test "test that the quantity is returned correctly" do
+    assert_equal 5, carts(:one).get_quantity
+  end
+
+  test "test that restaurant name returned" do
+    assert_equal restaurants(:one), carts(:one).restaurant
+  end
+
+  test "test that carts which are below the minimum spend are identified" do
+    assert_equal false, carts(:one).is_below_minimum_spend
+  end
 end
